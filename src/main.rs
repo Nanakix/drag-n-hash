c01527e87cd840f8e72f2f1740d76d5fa354fd3f
Nanakix <goldensun2000@hotmail.fr>
2024-03-28 22:15:57 +0100

    Cleaned display: On drag, display file infos in a Text container

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,10 @@ use std::fs::File;
 use std::io::Read;
 use humansize::{FormatSize, BINARY};
 use iced::alignment;
+// use iced::clipboard;
 use iced::event::{self, Event};
 use iced::executor;
-use iced::widget::{button, checkbox, container, text, Column};
+use iced::widget::{button, /*checkbox,*/ container, text, Column};
 use iced::window;
 use iced::{
     Alignment, Application, Command, Element, Length, Settings, Subscription,
@@ -13,13 +14,23 @@ use iced::{
 use md5::Md5;
 use sha1_smol::Sha1;
 use sha2::{Digest, Sha256};
+// use std::env;
+// use std::path::Path;
+// use std::ptr::null_mut;
+
 
 pub fn main() -> iced::Result {
+    // let args: Vec<String> = env::args().collect();
+    // let mut dragged_at_launch= null_mut();
+    // if !args.is_empty() && args.len() <= 1 {
+    //     dragged_at_launch =
+    // }
     Events::run(Settings {
         window: window::Settings {
             exit_on_close_request: false,
             ..window::Settings::default()
         },
+
         ..Settings::default()
     })
 }
@@ -27,8 +38,6 @@ pub fn main() -> iced::Result {
 #[derive(Debug, Default)]
 struct Events {
     rom_name: String,
-    last: Vec<Event>,
-    enabled: bool,
     md5: String,
     sha1: String,
     sha256: String,
@@ -40,10 +49,10 @@ struct Events {
 #[derive(Debug, Clone)]
 enum Message {
     EventOccurred(Event),
-    Toggled(bool),
+    // CopyToClipboard(Event),
+    // Toggled(bool),
     Exit,
 }
-
 impl Application for Events {
     type Message = Message;
     type Theme = Theme;
@@ -55,21 +64,11 @@ impl Application for Events {
     }
 
     fn title(&self) -> String {
-        String::from("Events - Iced")
+        String::from("Drag-n-hash")
     }
 
     fn update(&mut self, message: Message) -> Command<Message> {
         match message {
-            Message::EventOccurred(event) if self.enabled => {
-
-                self.last.push(event);
-
-                if self.last.len() > 5 {
-                    let _ = self.last.remove(0);
-                }
-
-                Command::none()
-            }
             Message::EventOccurred(event) => {
                 if let Event::Window(id, window::Event::CloseRequested) = event
                 {
@@ -90,18 +89,21 @@ impl Application for Events {
                     self.sha256 = format!("SHA256: {:X}", Sha256::digest(buffer.clone()));
                     self.md5 = format!("MD5: {:X}", Md5::digest(buffer.clone()));
                     self.rom_size = format!("ROM Size: {} bytes, ({})", buffer.len(), buffer.len().format_size(BINARY));
-                    dbg!(self);
+
                     Command::none()
                 }
                 else {
                     Command::none()
                 }
             }
-            Message::Toggled(enabled) => {
-                self.enabled = enabled;
-
-                Command::none()
-            }
+            // Message::CopyToClipboard(_event) => {
+            //     todo!()
+            // }
+            // Message::Toggled(enabled) => {
+            //     self.enabled = enabled;
+            //
+            //     Command::none()
+            // }
             Message::Exit => window::close(window::Id::MAIN),
         }
     }
@@ -111,15 +113,19 @@ impl Application for Events {
     }
 
     fn view(&self) -> Element<Message> {
-        let events = Column::with_children(
-            self.last
+        let dropped = Column::with_children(
+            [self.rom_name.as_str(),
+                 self.md5.as_str(),
+                 self.sha1.as_str(),
+                 self.sha256.as_str(),
+                 self.crc32.as_str(),
+                 self.crc64.as_str(),
+                 self.rom_size.as_str()]
                 .iter()
-                .map(|event| text(format!("{event:?}")).size(30))
+                .map(|event| text(event.to_string()).size(20))
                 .map(Element::from),
         );
 
-        let toggle = checkbox("Listen to runtime events", self.enabled)
-            .on_toggle(Message::Toggled);
 
         let exit = button(
             text("Exit")
@@ -133,8 +139,8 @@ impl Application for Events {
         let content = Column::new()
             .align_items(Alignment::Center)
             .spacing(20)
-            .push(events)
-            .push(toggle)
+            .push(dropped)
+            // .push(toggle)
             .push(exit);
 
         container(content)
